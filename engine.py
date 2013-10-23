48728cc6fd644ca1b31f01324e7491e6429e86b5
StudentAdmin <studentadmin@studentadmin-Latitude-E5410.(none)>
2013-10-23 15:39:51 -0800

    Added engine.py.

---
+++ engine.py
@@ -0,0 +1,20 @@
+def get_choice(choices):
+    # This function takes in a dictionary of choices,
+    #   and returns the user's choice.
+    # The dictionary has the form:
+    #   {'choice_token': 'choice text', }
+    # The function forces the user to make one of the given choices,
+    #   or quit.
+    print("\nWould you like to: ")
+    for choice_token in choices:
+        print("[%s]: %s" % (choice_token, choices[choice_token]))
+    # Always offer the choice to quit.
+    print("[q]: Quit.")
+    choice_token = ''
+    while choice_token not in choices.keys():
+        choice_token = input("\nWhat is your choice? ")
+        choice_token = str(choice_token)
+        if choice_token == 'q':
+            print("Thanks for playing! Bye.")
+            sys.exit()
+    return choice_token
