dc2611c67cc284b82642f9c043d02d3ac5c935e0
Eric Matthes <ehmatthes@gmail.com>
2013-10-24 14:13:05 -0800

    Five secrets implemented in place.

--- phs_adventure.py
+++ phs_adventure.py
@@ -21,32 +21,44 @@ while 1:
 
     # Respond to first choice.
     if choice == '1':
-	    os.system('clear')
-	    print("You have entered!")
-	    print("You notice that there is new paint on the walls.")
-	    print("Would you like to: ")
-	    print("\n[1] Open the door to Hillary's classroom.")
-	    print("[2] Go sit by Phil's desk.")
-	    print("[3] Go into Eric's class to do some Python.")
-	    choice = input("\nWhat is your choice? ")
-	    if choice == '1':
-		    os.system('clear')
-		    print("A British-sounding woman has just shouted at you.")
-	    elif choice == '2':
-		    # phil's desk
-		    os.system('clear')
-		    print("You are standing in front of Phil's desk.")
-		    print("Phil is talking on the phone about giraffes in the sky.")
-		    print("Would you like to: ")
-		    print("\n[1]Keep listening for a while.")
-		    print("[2]Interrupt Phil's conversation.")
-		    print("[3]Quietly sneak a Hames pass off of Phil's desk.")
-		    choice = input("\nWhat is your choice?")
-	    elif choice == '3':
-		    # eric's room
-		    pass
-	    else:
-		    print("I don't understand.")
+        os.system('clear')
+        
+        phs_values = ['honesty', 'perseverance', 'humor', 'acceptance', 'respect']
+        key = ''
+        while key.lower() not in phs_values:
+            os.system('clear')
+            print("You must know one of the five secrets in order to enter.")
+            key = input("Please tell me one of the secrets: ")
+        
+        # You know the secret!
+        os.system('clear')
+        
+        
+        print("You have entered!")
+        print("You notice that there is new paint on the walls.")
+        print("Would you like to: ")
+        print("\n[1] Open the door to Hillary's classroom.")
+        print("[2] Go sit by Phil's desk.")
+        print("[3] Go into Eric's class to do some Python.")
+        choice = input("\nWhat is your choice? ")
+        if choice == '1':
+    	    os.system('clear')
+    	    print("A British-sounding woman has just shouted at you.")
+        elif choice == '2':
+    	    # phil's desk
+    	    os.system('clear')
+    	    print("You are standing in front of Phil's desk.")
+    	    print("Phil is talking on the phone about giraffes in the sky.")
+    	    print("Would you like to: ")
+    	    print("\n[1]Keep listening for a while.")
+    	    print("[2]Interrupt Phil's conversation.")
+    	    print("[3]Quietly sneak a Hames pass off of Phil's desk.")
+    	    choice = input("\nWhat is your choice?")
+        elif choice == '3':
+    	    # eric's room
+    	    pass
+        else:
+    	    print("I don't understand.")
     elif choice == '2':
         os.system('clear')
         print("You drowned! Just kidding.")
